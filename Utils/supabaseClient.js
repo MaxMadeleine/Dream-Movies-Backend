76471129c53e2a055fe64fbb2daeea7600fc5e35
MaxMadeleine <Max.Madeleine.G@gmail.com>
2025-03-23 15:28:51 +0100

    Update supabaseClient.js to use hardcoded Supabase URL and key for testing

--- Utils/supabaseClient.js
+++ Utils/supabaseClient.js
@@ -3,4 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 import dotenv  from "dotenv";
 dotenv.config();
 
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);   
+export const supabase = createClient(
+    "https://qrsjogotplihbitfuuii.supabase.co/",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyc2pvZ290cGxpaGJpdGZ1dWlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIxMjc1OTIsImV4cCI6MjA1NzcwMzU5Mn0.6cf1jinIB0v78zDFRibkaTWY_N4p6bLqHKmi6c5nc2o"
+);
+
+
+//export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);   
